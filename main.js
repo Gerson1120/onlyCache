a93bad6ff13c9f26d3e40cd17fa41280a999411b
Gerson1120 <20223tn144@utez.edu.mx>
2025-10-24 11:13:25 -0600

    cambios

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(reg => console.log('[App] Registro SW exitoso:', reg))
             .catch(err => console.error('[App] Error de registro SW:', err));
     });
